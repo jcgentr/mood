e9e8e9e5f613ec0248707b6c13d82a2c2612acb7
Jared Gentry <jcgentr@clemson.edu>
2024-01-09 13:06:26 -0600

    Add new color theme; add logo to side bar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,7 +2,6 @@ import { UserButton } from '@clerk/nextjs'
 import Link from 'next/link'
 
 const links = [
-  { href: '/', label: 'Home' },
   { href: '/journal', label: 'Journal' },
   { href: '/history', label: 'History' },
 ]
@@ -15,12 +14,46 @@ export default function DashboardLayout({
   return (
     <div className="h-screen w-screen grid grid-cols-[200px,1fr]">
       <aside className="col-span-1 border-r border-black/10">
-        <div>Mood</div>
+        <Link href="/">
+          <svg
+            width="200px"
+            height="100px"
+            viewBox="0 0 300 100"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="100%" height="100%" fill="rgb(255, 247, 225)" />
+
+            <text
+              x="50%"
+              y="50%"
+              dominant-baseline="middle"
+              text-anchor="middle"
+              font-family="Verdana"
+              font-size="30"
+              fill="hsl(232, 26%, 50%)"
+              style={{ fontWeight: 'bold' }}
+            >
+              mood
+            </text>
+
+            <circle cx="150" cy="70" r="10" fill="hsl(232, 26%, 50%)" />
+            <circle cx="142" cy="67" r="2" fill="rgb(255, 247, 225)" />
+            <circle cx="158" cy="67" r="2" fill="rgb(255, 247, 225)" />
+            <path
+              d="M 145,73 Q 150,77 155,73"
+              stroke="rgb(255, 247, 225)"
+              stroke-width="1"
+              fill="none"
+            />
+          </svg>
+        </Link>
         <ul>
           {links.map((link) => (
-            <li key={link.label} className="px-2 py-6 text-xl">
-              <Link href={link.href}>{link.label}</Link>
-            </li>
+            <Link key={link.label} href={link.href}>
+              <li className="mx-4 p-2 rounded text-xl hover:bg-custom-color-trans">
+                {link.label}
+              </li>
+            </Link>
           ))}
         </ul>
       </aside>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'custom-color': 'hsla(232, 26%, 50%, 1)',
+        'custom-color-trans': 'hsla(232, 26%, 50%, 0.2)',
+      },
     },
   },
   plugins: [],
